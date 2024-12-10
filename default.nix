1c1b6aba4500b972d40f0be379a3bb8f036e7117
rogueking <miguel@muniz.org>
2024-12-10 11:34:00 +0000

    fixed revision

--- default.nix
+++ default.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchFromGitHub }:
 
 stdenv.mkDerivation rec {
-  name = "oh-my-zsh-${version}";
+  name = "oh-my-zsh";
   version = "1.0.0";
   src = fetchFromGitHub {
     # path to the upstream repository
     owner = "Rogue-King";
     repo = "ohmyzsh";
-    rev = "v${version}";
+    rev = "master";
     sha256 = "sha256-pLHEn8N4pN8wmaThN9lqxHG62G+6X3hxWEmvQbl1Dbs=";
   };
 
